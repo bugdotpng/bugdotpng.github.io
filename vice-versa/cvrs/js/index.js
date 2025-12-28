1d6192b8486e880cb5c7dc530333809bb4b818c0
unknown <haileytxt@gmail.com>
2025-12-28 12:31:37 -0600

    removed old cvrs repo from under vice versa

--- vice-versa/cvrs/js/index.js
+++
@@ -1,7 +0,0 @@
-function menuToggle() {
-  var toggle = document.getElementById("menu-toggle");
-  toggle.classList.toggle("active");
-  
-  var dropdown = document.getElementById("menu-dropdown");
-  dropdown.classList.toggle("active");
-}
